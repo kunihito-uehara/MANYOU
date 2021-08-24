d199e693b8e586de610fd51789ba304844b8476e
kunihitouehara <uekuni2000@gmail.com>
2021-08-24 09:07:54 +0900

    userテーブル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-
   resources :tasks
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show, :edit, :update]
+  namespace :admin do
+    resources :users
+  end
 end
